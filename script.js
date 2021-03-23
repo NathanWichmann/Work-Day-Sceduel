8d9cfdd25fe7d58ac75adb725e23a3bd87715d92
Nathan Wichmann <nathanwicker@hotmail.com>
2021-03-23 18:30:31 -0400

    updates

--- script.js
+++ script.js
@@ -19,23 +19,36 @@ var fivePmEl = $('#5pm')
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
-    console.log("timeDisplayEl")
+    // console.log("timeDisplayEl")
     // console.log('MMM DD, YYYY [at] hh:mm:ss a')
   }
   //lets the page load and the once document is ready this funtion runs 
   $(document).ready(function () {
-    //connecting to the savebTEl and crwading the ecent 
-    $(".btn").on('click', function( ) {
+    //connecting to the saveBtnEl and crwading the ecent 
+    $(".saveBtn").on('click', function( ) {
+      console.log(this)
     
       //gets the user input and the hour and sends it to the local storage in a json string. local storage cant save objects
-      userInputEl = $(this).siblings(".form-control").val();
-      console.log("userInputEl")
+      userInputEl = $(this).siblings(".description").val();
+      console.log("userInputEl", userInputEl)
       
-      hourUsedEl = $(this).siblings(".input-group-text").val();
-      localStorage.setItem(hourUsedEl, json.stringify(userInputEl));
+      hourUsedEl = $(this).siblings(".description").attr("id");
+      console.log("hourUsedEl", hourUsedEl)
+      
+      localStorage.setItem(hourUsedEl, userInputEl);
+      
     
   });
   });
+
+  // $(document).on("pagecreate",function(){
+  //   $("#8am .description").val(localStorage.getItem("8am"));
+  //  });
+  
+  $("#9am").val(localStorage.getItem("9am"));
+
+
+  
 
   //this calls the displaytime function and shows every second on the screen 
   setInterval(displayTime, 1000);
